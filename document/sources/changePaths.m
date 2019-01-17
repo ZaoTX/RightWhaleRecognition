36609b9e5669720707fe54887c226b122ea56fd9
Ziyao He <he.ziyao.uni-konstanz.de>
2019-01-17 13:25:21 +0100

    make changePaths, cropHead work for everyone

--- document/sources/changePaths.m
+++ document/sources/changePaths.m
@@ -1,10 +1,10 @@
+%% changePaths in gTruth.mat
+% you need to select the folder which includes the original images
 load('gTruth.mat')
 temp = gTruth.imageFilename;
+joobinPath = '/Users/jubin/Matlab/all/imgs/imgs_train_whaleIDs';
 
-% For Ying
-new_paths = strrep(temp, '/Users/jubin/Matlab/all/imgs/imgs_train_whaleIDs', '/Users/wangying/Desktop/ML-PRO/label'); % change the path with new path
-
-% For Ziyao
-% new_paths = strrep(temp, '/Users/jubin/Matlab/all/imgs/imgs_train_whaleIDs¡¯, ¡®C:\semester5\MachineLearning\ProjectData\forDeep'); % change the paths with new
-
+ yourPath = uigetdir(cd,'select folder containing training images');
+ new_paths = strrep(temp, joobinPath, yourPath); % change the paths with new path
+ 
 gTruth.imageFilename = new_paths;
--- document/sources/cropHead.m
+++ document/sources/cropHead.m
@@ -1,27 +1,35 @@
-load('gTruth.mat');
+%% crop whale head and store them to selected folder.
 
 % Get the imagePath and make the corresponding whale_ID folder and save the image.
 imageRoute = gTruth.imageFilename;
 imageBoundingBoxes = gTruth.Head;
-
+local = uigetdir(cd,'select folder to save cropped images');
+cd(local);
+imgs_train_cropped = [local filesep 'imgs_train_cropped'];
+ if ~exist(imgs_train_cropped,'dir')
+         mkdir('imgs_train_cropped');
+ end
+ cd(imgs_train_cropped);
 for i = 1:size(imageRoute, 1)
-    whaleID_str = strsplit(imageRoute{i}, '/');
-    whaleID_str = whaleID_str{8};
-
-    % create a folder for each whale (if non existant)
-    if ~exist(whaleID_str,'dir')
-        mkdir(whaleID_str);
-    end
-
-    formatStr = '%s_%d.jpg';
-
-    % Crop Head with imageBoundingBoxes
-
-    img = imread(imageRoute{i});
-    imcropped = imcrop(img, imageBoundingBoxes{i});
-%     imshow(imcropped);
-    fileName = sprintf(formatStr, whaleID_str, i);
-    if ~exist(fileName, 'file')
-        imwrite(imcropped, [whaleID_str filesep fileName]);
-    end
+    whaleID_str = strsplit(imageRoute{i},  '/');
+    whaleID_str = char(whaleID_str(end-1));
+   
+    destfolder = [imgs_train_cropped filesep whaleID_str];
+     % create a folder for each whale (if non existant)
+      if ~exist(whaleID_str,'dir')
+          mkdir(whaleID_str);
+      end
+ 
+      formatStr = '%s_%d.jpg';
+ 
+%  Crop Head with imageBoundingBoxes
+  
+       img = imread(imageRoute{i});
+       imcropped = imcrop(img, imageBoundingBoxes{i});
+% %     imshow(imcropped);
+      fileName = char(sprintf(formatStr, whaleID_str,i));
+      fullFileName = fullfile(destfolder, fileName);
+      if ~exist(fullFileName, 'file')
+          imwrite(imcropped, fullFileName,'jpg');
+      end
 end
