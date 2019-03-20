ad3236e0c1cf7f591675cbd569ae8e6078224437
yingwang1728 <34481679+yingwang1728@users.noreply.github.com>
2019-03-20 11:02:04 +0100

    Update and rename cropHead.m to crop.m

--- document/sources/Preprocessing/crop.m
+++ document/sources/Preprocessing/crop.m
@@ -1,8 +1,27 @@
-%% crop whale head and store them to selected folder.
+%% When running this file, you have to select two locations:
+%  First, the 'imgs_train_whaleIDs'folder; 
+%  Second, the location to store the folder with cropped images.
 
-% Get the imagePath and make the corresponding whale_ID folder and save the image.
+%% changePaths in gTruth.mat
+
+% preconditions: download 'gTruth.mat' to the workspace.
+load('gTruth.mat')
+temp = gTruth.imageFilename;
+joobinPath = '/Users/jubin/Matlab/all/imgs/imgs_train_whaleIDs';
+
+%select the 'imgs_train_whaleIDs' folder on your computer
+yourPath = uigetdir(cd,'select the imgs_train_whaleIDs.');
+new_paths = strrep(temp, joobinPath, yourPath); % change the paths with the new path
+ 
+gTruth.imageFilename = new_paths;
+
+%% to crop whale head and store them to a 'imgs_train_cropped' folder.
+
+% Get the imagePath and create the corresponding whale_ID folder and save the image.
 imageRoute = gTruth.imageFilename;
 imageBoundingBoxes = gTruth.Head;
+
+% to select the location for 'imgs_train_cropped' folder.
 local = uigetdir(cd,'select folder to save cropped images');
 cd(local);
 imgs_train_cropped = [local filesep 'imgs_train_cropped'];
@@ -22,7 +41,7 @@ for i = 1:size(imageRoute, 1)
  
       formatStr = '%s_%d.jpg';
  
-%  Crop Head with imageBoundingBoxes
+%  Crop Head with imageBoundingBoxes(ROIs)
   
        img = imread(imageRoute{i});
        imcropped = imcrop(img, imageBoundingBoxes{i});
